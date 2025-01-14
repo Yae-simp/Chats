1c994e83d1e47d21a09706f700ff551140d86912
tardes <irenexingni@tutanota.com>
2025-01-14 18:11:57 +0100

    i can log in with mail and password and it's saved

--- CocoaPods/ViewController.swift
+++ CocoaPods/ViewController.swift
@@ -9,12 +9,57 @@ import UIKit
 import FirebaseAuth
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func createUser(_ sender: Any) {
+        Auth.auth().createUser(withEmail: usernameTextField.text!, password: passwordTextField.text!) { authResult, error in
+            if let error = error {
+                // Hubo un error
+                print(error)
+                
+                let alertController = UIAlertController(title: "Create user", message: error.localizedDescription, preferredStyle: .alert)
 
+                alertController.addAction(UIAlertAction(title: "OK", style: .default))
+
+                self.present(alertController, animated: true, completion: nil)
+            } else {
+                // Todo correcto
+                print("User signs up successfully")
+                
+                let alertController = UIAlertController(title: "Create user", message: "User created successfully", preferredStyle: .alert)
+
+                alertController.addAction(UIAlertAction(title: "OK", style: .default))
+
+                self.present(alertController, animated: true, completion: nil)
+            }
+        }
+    }
+    
+    @IBAction func signIn(_ sender: Any) {
+        Auth.auth().signIn(withEmail: usernameTextField.text!, password: passwordTextField.text!) { [unowned self] authResult, error in
+          //guard let strongSelf = self else { return }
+            if let error = error {
+                // Hubo un error
+                print(error)
+                
+                let alertController = UIAlertController(title: "Sign In", message: error.localizedDescription, preferredStyle: .alert)
+
+                alertController.addAction(UIAlertAction(title: "OK", style: .default))
+
+                self.present(alertController, animated: true, completion: nil)
+            } else {
+                // Todo correcto
+                print("User signs in successfully")
+                self.performSegue(withIdentifier: "goToHome", sender: nil)
+            }
+        }
+    }
 }
 
